f65491d9ffda7ea624adaf868955e8a0c9ebcd89
Aniket Jha <aniketkumarj@gmail.com>
2020-06-24 20:26:49 +0530

    Update 322. Coin Change.cpp

--- 322. Coin Change.cpp
+++ 322. Coin Change.cpp
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/coin-change/
+
 
 class Solution {
 public:
@@ -26,5 +28,28 @@ public:
         
         
         
+    }
+};
+
+
+//Top Down
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int dp[amount+1];
+        dp[0] = 0;
+        for(int i=1;i<=amount;i++)
+        {
+            dp[i] = 1e9;
+            for(int j=0;j<n;j++)
+            {
+                if(coins[j]<=i)
+                    dp[i] = min(dp[i],1+dp[i-coins[j]]);
+            }
+        }
+        
+        return dp[amount]==1e9?-1:dp[amount];
     }
 };
